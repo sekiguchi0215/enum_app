53f9d437f111b70d465895dd47a89a9a7f920935
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-18 15:50:28 +0900

    管理者画面でも enums.ja.yml で設定した表示名を利用できるように変更

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -14,5 +14,40 @@ ActiveAdmin.register Post do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+  permit_params :genre, :content
+
+  index do
+    select_column
+    id_column
+    column :genre, :post, &:genre_i18n
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :genre, :post, &:genre_i18n
+      row :content
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      input :genre, as: :select, collection: Post.genres_i18n.invert
+      input :content
+    end
+    f.actions
+  end
+
+  filter :genre, as: :select, collection: Post.genres_i18n.invert.transform_values { |v| Post.genres[v] }
+  filter :content
+  filter :created_at
+  filter :updated_at
+
 end
